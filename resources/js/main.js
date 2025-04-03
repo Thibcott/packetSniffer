23fa53da70f5268e56fc9a6b0da8f301911ccdec
Thibcott <thibcott@outlook.com>
2025-04-03 09:55:09 -0600

    fix: remove fullscreen option from terminal command

--- resources/js/main.js
+++ resources/js/main.js
@@ -902,7 +902,7 @@ async function accessFileExplorer() {
  */
 async function accessTerminal() {
     try {
-        await Neutralino.os.execCommand('lxterminal --geometry=80x24 --fullscreen');
+        await Neutralino.os.execCommand('lxterminal --geometry=80x24');
     } catch (err) {
         console.error(`Error opening terminal: ${err.message} (${err.name})`);
     }
