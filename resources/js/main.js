6288bb15062fa2485b12ce640408644493f34d1b
Thibcott <thibcott@outlook.com>
2025-04-04 11:00:38 -0600

    fix: disable alwaysOnTop setting and remove unused logout function from main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -852,47 +852,6 @@ async function toggleFullScreen() {
 }
 
 
-
-/**
- * Logs out the user by locking the screen using the system's display manager tool.
- * This function executes the 'dm-tool lock' command to lock the screen.
- * 
- * @async
- * @function logout
- * @throws {Error} If an error occurs while attempting to lock the screen, 
- *         it will be logged to the console with the error message and name.
- */
-async function logout() {
-    try {
-        // Add the logout option to the system tray menu
-        let tray = {
-            icon: "/resources/icons/trayIcon.png",
-            menuItems: [
-                { id: "LOGOUT", text: "Logout" },
-                { id: "SEP", text: "-" },
-                { id: "QUIT", text: "Quit" }
-            ]
-        };
-
-        Neutralino.os.setTray(tray);
-
-        // Handle the logout action when the tray menu item is clicked
-        Neutralino.events.on("trayMenuItemClicked", async (event) => {
-            if (event.detail.id === "LOGOUT") {
-                try {
-                    let result = await Neutralino.os.execCommand('sudo pkill -u pi');
-                    console.log("Command executed successfully:", result.stdOut);
-                } catch (error) {
-                    console.error("Error executing command:", error);
-                }
-            }
-        });
-    } catch (err) {
-        console.error(`Error setting up logout in tray: ${err.message} (${err.name})`);
-    }
-}
-
-
 async function rebootRPI() {
     try {
         await Neutralino.os.execCommand('sudo reboot');
