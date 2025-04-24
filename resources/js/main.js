081ffdda3cc43b3de54745809f6e13403782b648
Thibcott <thibcott@outlook.com>
2025-04-24 09:40:05 -0600

    fix: remove unused table cell in file backup display for cleaner layout

--- resources/js/main.js
+++ resources/js/main.js
@@ -943,7 +943,6 @@ async function getFileFromBackupFolder() {
             let cell2 = row.insertCell(1);
             let cell3 = row.insertCell(2);
             let cell4 = row.insertCell(3);
-            let cell5 = row.insertCell(4);
 
             cell1.innerHTML = file.date;
             cell2.innerHTML = file.name.split('_')[0];
